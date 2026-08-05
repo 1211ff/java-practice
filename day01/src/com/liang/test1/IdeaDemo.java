30353649be9ff7e173735e1e330c7d79a8ddd9aa
1211ff <3358334147@qq.com>
2026-08-05 17:16:33 +0800

    chore: 收录 IdeaDemo 练习代码，新增 .gitignore

---
+++ day01/src/com/liang/test1/IdeaDemo.java
@@ -0,0 +1,25 @@
+package com.liang.test1;
+
+import java.util.Scanner;
+
+public class IdeaDemo {
+    public static void main(String[] args) {
+
+        /*String name="梁中瑜";
+
+
+        System.out.println(name);
+        // 打印当前JVM编码，用来校验是否生效
+        char b=456;*/
+        int b=666;
+        System.out.println(b/100);
+        System.out.println(b%100/10);
+        System.out.println(b%100%10);
+        System.out.println(false);
+
+
+
+
+
+    }
+}
